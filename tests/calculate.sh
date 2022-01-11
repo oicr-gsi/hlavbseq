470b0777a8b54acb77c445fe20e683b38aae786d
pruzanov <pruzanov@oicr.on.ca>
2022-01-11 13:36:04 -0500

    Slight Changes to calculate.sh script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -3,5 +3,5 @@ cd $1
 
 # We have three output txt files, files with raw and formatted predictions
 
-echo ".txt file:"
-for c in *txt;do md5sum $c;done 
+echo ".txt files:"
+for t in *.txt;do md5sum $t;done 
